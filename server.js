f7b16fcbb15bf8d061b4238351783a8ff2e54ffe
DP_Bell <smaj664@gmail.com>
2020-04-15 20:21:15 +0900

    7강: Add Node.js Express Server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "hello Express!" });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
