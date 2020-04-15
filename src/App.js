e6cae116f0f271b9b490cb0d595b5930e9737058
DP_Bell <smaj664@gmail.com>
2020-04-15 16:49:33 +0900

    5강: 고객 컴포넌트 구조화(map사용)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customer from './components/Customer';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
 
-const customer = {
-  name: "문민종",
-  birthday: 950608,
-  gender: "남자",
-  job: "대학생"
-}
+const customers = [
+  {
+    id: 0,
+    image: "https://placeimg.com/64/64/1",
+    name: "문민종",
+    birthday: 950608,
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/2",
+    name: "원재문",
+    birthday: 951212,
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/3",
+    name: "한희정",
+    birthday: 960912,
+    gender: "여자",
+    job: "직장인",
+  },
+];
 
-function App() {
-  return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="gray-background">
+        {customers.map((current) => {
+          return (
+            <Customer
+              key={current.id}
+              id={current.id}
+              image={current.image}
+              name={current.name}
+              birthday={current.birthday}
+              gender={current.gender}
+              job={current.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
